e19cc9932a950d33c2bdee7d9cee0a5810f8bf32
Maximiliano Ezequiel Afonso <mecsafonso@gmail.com>
2020-06-05 14:01:18 -0300

    Punto 1

--- Hierbas&Ratones/src/Lib.hs
+++ Hierbas&Ratones/src/Lib.hs
@@ -3,3 +3,22 @@ import Text.Show.Functions
 
 laVerdad = True
 
+type Enfermedad = String
+type Nombre = String
+
+data Raton = UnRaton {
+    nombre :: Nombre,
+    edad :: Int,
+    peso :: Float,
+    enfermedades :: [Enfermedad]
+} deriving (Eq, Show)
+
+
+-- PUNTO 1 
+    --- Modelar a los ratones 
+cerebro :: Raton
+cerebro = UnRaton "Cerebro" 9 0.2 ["Brucelosis", "Sarampión","Tuberculosis"]
+bicenterrata :: Raton
+bicenterrata = UnRaton "Bicenterrata" 256 0.2 []
+huesudo :: Raton
+huesudo = UnRaton "Huesudo" 4 10 ["Obesidad","Sinusitis"]
