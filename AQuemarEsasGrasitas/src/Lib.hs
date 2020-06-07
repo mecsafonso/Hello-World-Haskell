dab8f4cf726c0ad9235e583ece0f6b37f8628124
Maximiliano Ezequiel Afonso <mecsafonso@gmail.com>
2020-06-07 15:44:55 -0300

    Punto 3

--- AQuemarEsasGrasitas/src/Lib.hs
+++ AQuemarEsasGrasitas/src/Lib.hs
@@ -50,3 +50,43 @@ perderPeso gimnasta cantidadPerdida = gimnasta {peso = peso gimnasta - cantidadP
 
 esViejo :: Gimnasta -> Bool
 esViejo = (>30).edad
+
+-- PUNTO 3
+-- Desarrollar las funciones para los ejercicios caminataEnCinta, entrenamientoEnCinta, pesas, colina y montania sabiendo que:
+
+-- La cinta quema calorías en función de la velocidad promedio alcanzada durante el ejercicio, quemando 1 caloría por la velocidad promedio por minuto.
+    -- La caminata es un ejercicio en cinta con velocidad constante de 5 km/h. 
+
+caminataEnCinta :: Ejercicio
+caminataEnCinta duracion gimnasta = quemarCalorias gimnasta (1*5*duracion)
+
+    -- El entrenamiento en cinta arranca en 6 km/h y cada 5 minutos incrementa la velocidad en 1 km/h, con lo cual la velocidad máxima dependerá de los 
+    -- minutos de entrenamiento.
+
+entrenamientoEnCinta :: Ejercicio
+entrenamientoEnCinta duracion gimnasta = quemarCalorias gimnasta (caloriasQuemadasDelEntrenamiento duracion)
+    where caloriasQuemadasDelEntrenamiento duracion = 1* ((6 + (velocidadMaximaAlcanzada duracion) ) / 2 ) * duracion
+
+velocidadMaximaAlcanzada = (+6).(/5)
+
+-- Las pesas tonifican la décima parte de los kilos a levantar si se realiza por más de 10 minutos, sino nada. 
+
+pesas :: Float -> Ejercicio
+pesas kilos duracion gimnasta | duracion > 10 = tonificar (tonif gimnasta + (kilos * 0.1)) gimnasta 
+    | otherwise = gimnasta
+
+tonificar :: Float -> Gimnasta  -> Gimnasta
+tonificar cantidad  gimnasta  = gimnasta {tonif = tonif gimnasta + cantidad}
+-- la colina quema 2 calorías por minuto multiplicado por la inclinación de la colina. 
+
+colina :: Float -> Ejercicio
+colina inclinacion duracion = flip quemarCalorias (2*duracion*inclinacion)
+
+
+-- La montaña son 2 colinas sucesivas (cada una con la mitad de duración respecto de los minutos totales indicados), 
+-- donde la segunda colina tiene una inclinación de 3 más que la inclinación inicial elegida. 
+-- Además de hacer perder peso por las calorías quemadas por las colinas, este ejercicio incrementa en una unidad la tonificación de la gimnasta. 
+    -- Resolver usando composición y aplicación parcial.
+
+montaña :: Float -> Ejercicio
+montaña inclinacionInicial duracion  = tonificar 1 . colina (inclinacionInicial + 3)  (duracion/2) . colina inclinacionInicial ( duracion/2 )  
