e20bf67fc02be22dcfaf8ba44b8b0ced480aca5a
Maximiliano Ezequiel Afonso <mecsafonso@gmail.com>
2020-06-09 18:27:36 -0300

    Punto 2

--- FMI/src/Lib.hs
+++ FMI/src/Lib.hs
@@ -13,8 +13,8 @@ type Recurso = String
 data Pais = UnPais {
     nombre :: String,
     ingresoPC :: Float,
-    poblacionSPub :: Int,
-    poblacionSPri :: Int,
+    poblacionSPub :: Float,
+    poblacionSPri :: Float,
     recursos :: [Recurso],
     deudaFMI :: Float
 }
@@ -30,3 +30,58 @@ namibia = UnPais {
     deudaFMI = 50
 }
 
+-- PUNTO 2
+
+type Estrategia = Pais -> Pais
+
+-- prestarle n millones de dólares al país, esto provoca que el país se endeude en un 150% de lo que el FMI le presta (por los intereses)
+
+prestarleNMill :: Float -> Estrategia
+prestarleNMill n = endeudarPaisPorcentual 150 n 
+
+endeudarPaisPorcentual :: Float -> Float -> Pais -> Pais
+endeudarPaisPorcentual porcentaje n pais = pais {deudaFMI = deudaFMI pais + (porcentajeReal porcentaje * n)}
+
+porcentajeReal x = x * 0.01
+
+
+-- reducir x cantidad de puestos de trabajo del sector público, lo que provoca que se reduzca la cantidad de activos en el sector público y 
+-- además que el ingreso per cápita disminuya en 20% si los puestos de trabajo son más de 100 ó 15% en caso contrario
+
+reducirPuestosSP :: Estrategia
+reducirPuestosSP pais | poblacionSPub pais > 100 = efectoReducirPuestosSP 20 pais
+    | otherwise = efectoReducirPuestosSP 15 pais
+
+efectoReducirPuestosSP :: Float -> Pais -> Pais
+efectoReducirPuestosSP n = disminuirIngresosPC n . disminuirPoblaciónSPub n 
+
+disminuirPoblaciónSPub n pais = pais { poblacionSPub = poblacionSPub pais * (porcentajeReal n)}
+
+disminuirIngresosPC n pais = pais { ingresoPC = ingresoPC pais * (porcentajeReal n) }
+
+
+-- darle a una empresa afín al FMI la explotación de alguno de los recursos naturales, esto disminuye 2 millones de dólares la deuda que el 
+-- país mantiene con el FMI pero también deja momentáneamente sin recurso natural a dicho país. No considerar qué pasa si el país no tiene dicho recurso.
+
+darRN :: Recurso -> Estrategia
+darRN recurso = perderRecurso recurso . disminuirDeuda 2
+
+perderRecurso :: Recurso -> Pais -> Pais
+perderRecurso recurso pais = pais {recursos = filter ( /= recurso) (recursos pais)}
+
+disminuirDeuda :: Float -> Pais -> Pais
+disminuirDeuda n pais = pais {deudaFMI = deudaFMI pais - n}
+
+
+-- establecer un “blindaje”, lo que provoca prestarle a dicho país la mitad de su Producto Bruto Interno (que se calcula como el ingreso per cápita 
+-- multiplicado por su población activa, sumando puestos públicos y privados de trabajo) y reducir 500 puestos de trabajo del sector público. 
+-- Evitar la repetición de código.
+
+blindaje :: Estrategia
+blindaje = prestamoPBI . disminuirPoblaciónSPub' 500
+
+prestamoPBI :: Pais -> Pais
+prestamoPBI pais = undefined
+
+disminuirPoblaciónSPub' :: Float -> Pais -> Pais
+disminuirPoblaciónSPub' n pais = pais {poblacionSPub = poblacionSPub pais - n}
